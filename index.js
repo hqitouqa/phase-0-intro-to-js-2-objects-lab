a8abe4f7961f6ed361ebb77ccfc3720edd3ed0a4
Hala Qitouqa <h.qitouqa@gmail.com>
2023-09-27 21:16:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee={
+    'name': 'Yara',
+    'streetAddress': 'Amm'
+}
+function updateEmployeeWithKeyAndValue(employee,name,streetAddress){
+    const newEmployee= {...employee};
+    newEmployee.name='Sam';
+    newEmployee.streetAddress='11 Broadway'
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,name,streetAddress){
+    employee.name='Sam';
+    employee.streetAddress='12 Broadway'
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,name){
+    const newEmployee={...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,name){
+    delete employee.name;
+    return employee;
+}
